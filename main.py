505e599459ce28c65678adf92a96d584afc26ee3
Kosuke Yamazaki <cnj19040@ict.nitech.ac.jp>
2024-06-20 12:40:14 +0900

    change cap

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_image():
 
     st.image(
         im,
-        caption='image from wikimedia commons, now at 2024/06/20/12:25',
+        caption='image from wikimedia commons, now at 2024/06/20/12:39',
         use_column_width=True
     )
 
